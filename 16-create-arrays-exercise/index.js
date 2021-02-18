d54b7460b588a76cc9f4972897c73d8747dc147c
Ayrton Haider <ayrtonhaider1608@gmail.com>
2021-02-18 07:25:51 +0000

    16-create-arrays-exercise

--- 16-create-arrays-exercise/index.js
+++ 16-create-arrays-exercise/index.js
@@ -32,6 +32,10 @@ let animals = wildAnimals.concat(pets);
  * find dog index in "animals" array
  */
 
+let dogIndex = animals.findIndex((animal) => {
+  return animal === "dog";
+});
+
 /**
  * Exercise 5
  *
@@ -39,11 +43,16 @@ let animals = wildAnimals.concat(pets);
  * find panda index in "animals" array
  */
 
+let pandaIndex = animals.indexOf("panda");
+console.log(pandaIndex);
+
 /**
  * create a variable with name {numbers}  which will be an array
  * and contain numbers from 1 to 9(ascending) and from 9 to 1(descending)
  * (each number should be twice in this array).
  */
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 
 /**
  * Exercise 6
@@ -52,12 +61,19 @@ let animals = wildAnimals.concat(pets);
  * find index of first instance of digit 4
  */
 
+let first4DigitIndex = numbers.findIndex((number) => {
+  return number === 4;
+});
+console.log(first4DigitIndex);
 /**
  * Exercise 7
  *
  * Create variable {ast4DigitIndex} and from {numbers}
  * find index of last instance of digit 4
  */
+
+let last4DigitIndex = numbers.lastIndexOf(4);
+console.log(last4DigitIndex);
 
 /**
  * Exercise 8
@@ -66,6 +82,8 @@ let animals = wildAnimals.concat(pets);
  * contains 7
  */
 
+let hasSeven = numbers.includes(7);
+console.log(hasSeven);
 /**
  * Exercise 9
  *
@@ -73,6 +91,8 @@ let animals = wildAnimals.concat(pets);
  * contains 20
  */
 
+let hasTwenty = numbers.includes(20);
+console.log(hasTwenty);
 /**
  * Exercise 10
  *
@@ -80,9 +100,17 @@ let animals = wildAnimals.concat(pets);
  * from {numbers} that is more than 4
  */
 
+let moreThanFour = numbers.findIndex((number) => {
+  return number > 5; // Does this require the index number or the value??
+});
+console.log(moreThanFour);
 /**
  * Exercise 11
  *
  * Create variable {moreEqualThanSeven} returns first number
  * from {numbers} that are more or equal 7
  */
+let moreEqualThanSeven = numbers.findIndex((number) => {
+  return number >= 7;
+});
+console.log(moreEqualThanSeven);
