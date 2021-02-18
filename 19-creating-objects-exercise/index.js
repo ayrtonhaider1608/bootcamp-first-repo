2511ba2613de74381cbf01e866129f06925c9471
Ayrton Haider <ayrtonhaider1608@gmail.com>
2021-02-18 08:06:20 +0000

    19-creating-objects-exercise

--- 19-creating-objects-exercise/index.js
+++ 19-creating-objects-exercise/index.js
@@ -7,6 +7,12 @@
  * PS: use console.log() to see the result
  */
 
+let user = {
+  firstName: "Ayrton",
+  lastName: "Haider",
+  age: 27,
+};
+
 /**
  * Exercise 2
  *
@@ -15,6 +21,20 @@
  *
  * PS: use console.log() to see the result
  */
+
+let allTypes = {
+  string: "Hello, World",
+  number: 42,
+  weekday: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+  anotherObject: {
+    key: 1,
+    key2: 2,
+  },
+  nestedFunction: function message() {
+    console.log(message());
+  },
+  boolean: 5 === 5,
+};
 
 /**
  * Exercise 3
@@ -25,3 +45,11 @@
  *
  * PS: use console.log() to see the result
  */
+
+function createMovieObject(name, rating, ticketPrice) {
+  return (movie = {
+    name: "Home Alone",
+    rating: 8.5,
+    ticketPrice: 20,
+  });
+}
